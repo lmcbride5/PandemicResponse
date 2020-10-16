8430b74d3d02c6906194aeb6963c52c71c6181ae
lmcbride5 <lilibeth.mcbride@patelhs.org>
2020-10-16 14:17:11 +0000

    I added a scanner for the user to input their city's number of cases and population.

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // User can input their cities population and number of cases they have 
+      int population = sc.nextInt(); 
+      int numberOfCases = sc.nextInt(); 
+    
+    // Next, the program find ths average number of cases the city has
+    double averageOfCases = numberOfCases / population;
+
+    //Then. 
+
+    if (averageOfCases >= 1000);
+      {
+        System.out.println("Send 10 experts"); 
+      }
+    else (averageOfCases >= 3000);
+      { 
+        System.out.println("Send 30 experts"); 
+      }
   }
 }
